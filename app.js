107b096c527d913166c4eef4300ee52554fe976b
henrybowman99 <95651156+henrybowman99@users.noreply.github.com>
2022-03-08 17:02:05 -0800

    Add function and button to filter table

--- app.js
+++ app.js
@@ -21,3 +21,23 @@ function buildTable(data){
     );
     });
 }
+
+//build function 
+function handleClick(){
+    //grab the date from the filter
+    let date = d3.select("#datetime").property("value");
+    //set filtered data to full table data by default
+    let filteredData = tableData;
+   //check to see if a date was entered and change the filtered data accordingly
+    if (date){
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+    // call buildTable function with the filtered data
+    buildTable(filteredData);
+
+};
+//direct handlClick to run when filter button is clicked
+d3.selectAll("filter-btn").on("click", handleClick);
+
+//make the full table data show up by default when page opens
+buildTable(tableData);
