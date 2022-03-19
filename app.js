0821bf53f0e4fd79475c0c10a3ab3508c5ea0202
Henry Bowman <habowman@wisc.edu>
2022-03-19 13:31:36 -0700

    Add files via upload
    Update code to filter table on city, state, country, and shape.

--- app.js
+++ app.js
@@ -1,43 +1,151 @@
-// import data 
+// from data.js
 const tableData = data;
 
-//use d3 to reference the HTML table
-var tbody = d3.select("tbody")
+// get table references
+var tbody = d3.select("tbody");
 
-function buildTable(data){
-    //Clear out any existing data
-    tbody.html("");
-    
-    //loop through data
+function buildTable(data) {
+  // clear out any existing data
+  tbody.html("");
 
-    data.forEach((dataRow) =>{
-        //create new row for each object in data
-        let row = tbody.append("tr");
-        //create new cell for each value and assign the cell the value
-        Object.values(dataRow).forEach((value) => {
-            let cell = row.append("td");
-            cell.text(value);
-    }
-    );
+  // loop through each object in the data and append a row and cells for each value in the row
+  data.forEach((dataRow) => {
+    // append a row to the table body
+    let row = tbody.append("tr");
+
+    // loop through each field in the dataRow and add
+    // each value as a table cell (td)
+    Object.values(dataRow).forEach((val) => {
+      let cell = row.append("td");
+      cell.text(val);
     });
+  });
 }
 
-//build function 
-function handleClick(){
-    //grab the date from the filter
-    let date = d3.select("#datetime").property("value");
-    //set filtered data to full table data by default
+//create filters variable
+var filters = {}
+
+
+//create function to update filters based on inputs 
+function updateFilters() {
+
+    // save the element that was changed as updatedElement variable
+    let updatedElement = d3.select(this);
+ 
+    //save the value of updated element in separate variable
+    let elementValue = updatedElement.property("value")
+    //  save the id of the filter that was changed as a variable.
+    let updatedElementId = updatedElement.attr("id")
+  
+    // if filter value was entered then add that filterId and value
+    // to the filters list. If not make sure that filter is removeed from the filters object.
+    if (elementValue){
+      filters[updatedElementId] = elementValue;
+    
+    }
+    else{
+      delete filters[updatedElementId];
+    }
+   
+    
+    
+    //call filterTable function to apply all filters and rebuild the table
+    filterTable();
+  };
+  
+  
+  //create function to filter the table when data is entered.
+  function filterTable() {
+  
+    //  default the filtered data to the tableData.
     let filteredData = tableData;
-   //check to see if a date was entered and change the filtered data accordingly
-    if (date){
-        filteredData = filteredData.filter(row => row.datetime === date);
-    };
-    // call buildTable function with the filtered data
+    // check if each filter is in filters object and keep any data that
+    // matches the filter values
+    //check if datetime is in filters and if it is filter the data on date
+    if ("datetime" in filters){
+      function filterdate(sightings){
+        
+          return sightings.datetime == filters.datetime;
+
+      }
+    
+    filteredData = filteredData.filter(filterdate);
+    //if not keep the data the same as it was
+    }else{
+      filteredData = filteredData;
+      
+    }
+    //check if city is in filters and if it is filter data on city
+    if ("city" in filters){
+      function filtercity(sightings){
+        
+          return sightings.city == filters["city"];
+
+      }
+    
+    filteredData = filteredData.filter(filtercity);
+    //if not keep the data the same
+    }else{
+      filteredData = filteredData;
+      
+    }
+    //check if state is in filters and if it is filter data on state
+    if ("state" in filters){
+      function filterstate(sightings){
+        
+          return sightings.state == filters["state"];
+
+      }
+    
+    filteredData = filteredData.filter(filterstate);
+    //if not keep the data the same
+    }else{
+      filteredData = filteredData;
+      
+    }
+    //check if country is in filters and if it is filter data on country
+    if ("country" in filters){
+      function filtercountry(sightings){
+        
+          return sightings.country == filters["country"];
+
+      }
+    
+    filteredData = filteredData.filter(filtercountry);
+    //if not keep the data the same
+    }else{
+      filteredData = filteredData;
+      
+    }
+    //check if shape is in filters and if it is filter data on shape
+    if ("shape" in filters){
+      function filtershape(sightings){
+        
+          return sightings.shape == filters["shape"];
+
+      }
+    
+    filteredData = filteredData.filter(filtershape);
+    //if not keep the data the same
+    }else{
+      filteredData = filteredData;
+      
+    }
+    
+    // rebuild the table using the filtered data
     buildTable(filteredData);
+ 
 
-};
-//direct handlClick to run when filter button is clicked
-d3.selectAll("filter-btn").on("click", handleClick);
+  
 
-//make the full table data show up by default when page opens
-buildTable(tableData);
+    }
+  
+    
+  
+  
+  // attach an event to listen for changes to each filter
+  d3.selectAll("input").on("change", updateFilters);
+  
+  // build table with full data when the table when the page loads
+  buildTable(tableData);
+  
